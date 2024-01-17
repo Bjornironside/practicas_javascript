510df47db81ad1a9e01ea6abad673ae572b4eb4a
bjornironside <tomaslyall97@gmail.com>
2024-01-17 17:08:17 -0300

    guardando

---
+++ test.js
@@ -0,0 +1,35 @@
+
+
+// Seleccionamos el botón por su id
+
+var divTest = document.getElementById("myDiv")
+
+var miBoton = document.getElementById("miBoton");
+
+var inputname = document.getElementById('input_name')
+
+var inputpass = document.getElementById('input_pass')
+
+
+
+
+miBoton.addEventListener("click", function () {
+   console.log(inputname.value)
+   
+   console.log(inputpass.value)
+   
+   
+   
+   
+   
+   
+   
+   
+   
+    // console.log(inputvalue.value)
+
+//    divTest.innerHTML = inputValue.value
+
+//    inputValue.type ="password"
+   
+});
