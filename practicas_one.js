4b627e8860c6f3f4f6203c2b8d7c16b5ff7a5538
martin <barreiromartinj@gmail.com>
2024-02-09 17:54:04 -0300

    Saving changes.

---
+++ practicas_one.js
@@ -0,0 +1,26 @@
+
+var one = 1
+
+var two = "2"
+
+var booleanoAux = true
+
+var undefinedAux = undefined
+
+var nombre = 'martin'
+
+var apellido = 'barreiro'
+
+
+const probando = () => {
+
+}
+
+probando()
+
+
+
+
+
+
+
--- test.js
+++ test.js
@@ -12,15 +12,26 @@ var inputpass = document.getElementById('input_pass')
 
 
 
-miBoton.addEventListener("click", function () {
+var lista = ['martin', 'tomy', 'belu']
+
+const login = () => {
     console.log(inputname.value)
     console.log(inputpass.value)
+    console.log(lista)
+}
+
+
+miBoton.addEventListener("click", function () {
+
+    login()
+
 
     // console.log(inputvalue.value)
     // divTest.innerHTML = inputValue.value
     // inputValue.type ="password"
 
-    alert(`nombre: ${inputname.value}  pass: ${inputpass.value}`)
+    // alert(`nombre: ${inputname.value}  pass: ${inputpass.value}`)
+    alert('Usuario logeado  o usuario no logeado')
 });
 
 
